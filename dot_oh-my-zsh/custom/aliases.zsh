76778709e56c22dff64dff46da1841a97ba2d8da
Marcelo Manfredo <marcelomanfredo@gmail.com>
2025-03-15 17:31:07 -0300

    Enhanced aliases

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,25 +1,22 @@
-# Aliases for system opts
-
-# Zsh
-alias zshconf="nvim ~/.local/share/chezmoi/dot_zshrc"
-alias zshalias="nvim ~/.local/share/chezmoi/dot_oh-my-zsh/custom/aliases.zsh"
-
-# Hyprland
-alias hyprconf="nvim ~/.local/share/chezmoi/dot_config/hypr/"
-
-# Waybar
-alias wayconf="nvim ~/.local/share/chezmoi/dot_config/waybar"
-
-# Foot
-alias footconf="nvim ~/.local/share/chezmoi/dot_config/foot/foot.ini"
-
 # Quality of Life
 alias c="clear -x"
 alias temp="watch -n 10 nvidia-smi"
+
+# Zsh
+alias zshconf="chezmoi edit ~/.zshrc"
+alias zshalias="chezmoi edit ~/.oh-my-zsh/custom/aliases.zsh"
+
+# Hyprland
+alias hyprconf="chezmoi edit ~/.config/hypr/"
+
+# Waybar
+alias wayconf="chezmoi edit ~/.config/waybar/"
+
+# Foot
+alias footconf="chezmoi edit ~/.config/foot/foot.ini"
 
 # Postgres
 alias psql="psql gondor"
 
 # Tmux
 alias tg="tmux new -s gondor -c ~/Projects/Gondor"
-alias tc="tmux new -s config -c ~/.local/share/chezmoi/dot_config"
