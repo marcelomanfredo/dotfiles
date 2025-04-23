53f6371b9ba54d8d47ed712cfe7eedd65d01043b
Marcelo Manfredo <marcelomanfredo@gmail.com>
2025-04-23 18:05:39 -0300

    Changed config colors for `catppuccin-machiatto` in `nvim/lua/custom/plugins/colors.lua`

--- dot_config/nvim/lua/custom/plugins/colors.lua
+++ dot_config/nvim/lua/custom/plugins/colors.lua
@@ -15,9 +15,16 @@ return {
         opts = {},
     },
     {
-        "catppuccin/nvim",
+        "catppuccin/nvim", --machiatto
         name = "catppuccin",
         lazy = false,
         opts = {},
+        config = function()
+            require('catppuccin').setup({
+                transparent_background = true,
+                show_end_of_buffer = true,
+                --term_colors = true,
+            })
+        end
     },
 }
