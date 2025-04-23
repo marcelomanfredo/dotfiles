532f4745578b1265d707073e14722776e1f5af8f
Marcelo Manfredo <marcelomanfredo@gmail.com>
2025-04-23 18:07:58 -0300

    Removed unecessary plugin usage in `colors.lua`

--- dot_config/nvim/lua/custom/plugins/colors.lua
+++ dot_config/nvim/lua/custom/plugins/colors.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        --Kanagawa
-        "rebelot/kanagawa.nvim",
-        lazy = false,
-        name = "kanagawa",
-        priority = 1000,
-        opts = {},
-    },
-    {
-        -- Tokionight
-        "folke/tokyonight.nvim",
-        name = "tokyonight",
-        lazy = false,
-        opts = {},
-    },
+    -- {
+    --     --Kanagawa
+    --     "rebelot/kanagawa.nvim",
+    --     lazy = false,
+    --     name = "kanagawa",
+    --     priority = 1000,
+    --     opts = {},
+    -- },
+    -- {
+    --     -- Tokionight
+    --     "folke/tokyonight.nvim",
+    --     name = "tokyonight",
+    --     lazy = false,
+    --     opts = {},
+    -- },
     {
         "catppuccin/nvim", --machiatto
         name = "catppuccin",
