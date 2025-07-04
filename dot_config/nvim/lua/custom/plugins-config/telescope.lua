0a89ae6ce80540d462f25b385dff1125545d5fe5
Marcelo Manfredo <marcelomanfredo@gmail.com>
2025-07-04 16:01:48 -0300

    fix(nvim): fix telescope keymap for "find files"

--- dot_config/nvim/lua/custom/plugins-config/telescope.lua
+++ dot_config/nvim/lua/custom/plugins-config/telescope.lua
@@ -29,7 +29,7 @@ local builtin = require "telescope.builtin"
 vim.keymap.set('n', '<leader>/', function()
     builtin.current_buffer_fuzzy_find({ sorting_strategy = "ascending", previewer = false, layout_config = { prompt_position = "top" } })
 end, { desc = "Search current buffer" })
-vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = "Find files in current work directory" })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files in current work directory" })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = "Find git files from current project" })
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Find commits from the project" })
 vim.keymap.set('n', '<leader>rg', function() builtin.live_grep({ cwd = vim.fn.expand('%:p:h') }) end,
@@ -37,7 +37,7 @@ vim.keymap.set('n', '<leader>rg', function() builtin.live_grep({ cwd = vim.fn.ex
 vim.keymap.set('n', '<leader>gr', function() builtin.grep_string({ search = vim.fn.input("Grep >> ") }) end,
     { desc = "Search string under the cursor/selection in current work directory" })
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Search all keymaps configured for normal mode" })
-vim.keymap.set('n', '<leader>ff',
-    function() builtin.find_files { cwd = vim.fn.expand("~/.local/share/chezmoi/dot_config/nvim") } end,
-    { desc = "Find file inside .config/nvim directory [CHEZMOI]" })
+vim.keymap.set('n', '<leader>fc',
+    function() builtin.find_files { cwd = vim.fn.expand("~/.local/share/chezmoi/dot_config") } end,
+    { desc = "Find file inside .config/ directory [CHEZMOI]" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Search help tags" })
